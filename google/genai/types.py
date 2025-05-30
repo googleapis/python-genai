9c2e3aeb319e2ff6ce59bd9b1a5b2321de35fd00
Google Team Member <genai-sdk-bot@google.com>
2025-05-29 18:30:54 -0700

    feat: RAG - Introduce configuration to corpus types, with DocumentCorpus and MemoryCorpus options.
    PiperOrigin-RevId: 764966276

--- google/genai/types.py
+++ google/genai/types.py
@@ -2596,6 +2596,10 @@ class VertexRagStore(_common.BaseModel):
       default=None,
       description="""Optional. Only return results with vector distance smaller than the threshold.""",
   )
+  store_context: Optional[bool] = Field(
+      default=False,
+      description="""Optional. Whether to store the context in the specified RAG corpus above. Currently only supported for Gemini Multimodal Live API.""",
+  )
 
 
 class VertexRagStoreDict(TypedDict, total=False):
@@ -2615,6 +2619,9 @@ class VertexRagStoreDict(TypedDict, total=False):
 
   vector_distance_threshold: Optional[float]
   """Optional. Only return results with vector distance smaller than the threshold."""
+
+  store_context: Optional[bool]
+  """Optional. Whether to store the context in the specified RAG corpus above. Currently only supported for Gemini Multimodal Live API."""
 
 
 VertexRagStoreOrDict = Union[VertexRagStore, VertexRagStoreDict]
