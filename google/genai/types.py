8d20935d56ff1877f3ad3158cb949107772584d7
Google Team Member <genai-sdk-bot@google.com>
2025-06-02 13:24:13 -0700

    feat: RAG - Introducing context storing for Gemini Live API.
    PiperOrigin-RevId: 766317763

--- google/genai/types.py
+++ google/genai/types.py
@@ -2636,6 +2636,10 @@ class VertexRagStore(_common.BaseModel):
       default=None,
       description="""Optional. Only return results with vector distance smaller than the threshold.""",
   )
+  store_context: Optional[bool] = Field(
+      default=False,
+      description="""Optional. Whether to store the context in the specified RAG corpus above. Currently only supported for Gemini Multimodal Live API.""",
+  )
 
 
 class VertexRagStoreDict(TypedDict, total=False):
@@ -2655,6 +2659,9 @@ class VertexRagStoreDict(TypedDict, total=False):
 
   vector_distance_threshold: Optional[float]
   """Optional. Only return results with vector distance smaller than the threshold."""
+
+  store_context: Optional[bool]
+  """Optional. Whether to store the context in the specified RAG corpus above. Currently only supported for Gemini Multimodal Live API."""
 
 
 VertexRagStoreOrDict = Union[VertexRagStore, VertexRagStoreDict]
