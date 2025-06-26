24d6c1f7b9be81ddd4f61870df6d60b09a3721fd
Frances Hubis Thoma <fth@google.com>
2025-06-26 12:51:30 -0700

    feat: Enable Vertex Multimodal Dataset as input to supervised fine-tuning.
    PiperOrigin-RevId: 776245674

--- google/genai/types.py
+++ google/genai/types.py
@@ -8721,6 +8721,10 @@ class TuningDataset(_common.BaseModel):
       default=None,
       description="""GCS URI of the file containing training dataset in JSONL format.""",
   )
+  vertex_dataset_resource: Optional[str] = Field(
+      default=None,
+      description="""The resource name of the Vertex Multimodal Dataset that is used as training dataset. Example: 'projects/my-project-id-or-number/locations/my-location/datasets/my-dataset-id'.""",
+  )
   examples: Optional[list[TuningExample]] = Field(
       default=None,
       description="""Inline examples with simple input/output text.""",
@@ -8732,6 +8736,9 @@ class TuningDatasetDict(TypedDict, total=False):
 
   gcs_uri: Optional[str]
   """GCS URI of the file containing training dataset in JSONL format."""
+
+  vertex_dataset_resource: Optional[str]
+  """The resource name of the Vertex Multimodal Dataset that is used as training dataset. Example: 'projects/my-project-id-or-number/locations/my-location/datasets/my-dataset-id'."""
 
   examples: Optional[list[TuningExampleDict]]
   """Inline examples with simple input/output text."""
@@ -8746,12 +8753,19 @@ class TuningValidationDataset(_common.BaseModel):
       default=None,
       description="""GCS URI of the file containing validation dataset in JSONL format.""",
   )
+  vertex_dataset_resource: Optional[str] = Field(
+      default=None,
+      description="""The resource name of the Vertex Multimodal Dataset that is used as training dataset. Example: 'projects/my-project-id-or-number/locations/my-location/datasets/my-dataset-id'.""",
+  )
 
 
 class TuningValidationDatasetDict(TypedDict, total=False):
 
   gcs_uri: Optional[str]
   """GCS URI of the file containing validation dataset in JSONL format."""
+
+  vertex_dataset_resource: Optional[str]
+  """The resource name of the Vertex Multimodal Dataset that is used as training dataset. Example: 'projects/my-project-id-or-number/locations/my-location/datasets/my-dataset-id'."""
 
 
 TuningValidationDatasetOrDict = Union[
