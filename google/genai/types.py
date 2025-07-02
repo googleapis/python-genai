554ac74fa564c278341d00fadfc6b14da525ee13
Jason Dai <jsndai@google.com>
2025-07-02 13:52:45 -0700

    feat: GenAI SDK client(evals) - Add support for rubric-based metrics, and rubric customization eval workflow
    PiperOrigin-RevId: 778630998

--- google/genai/types.py
+++ google/genai/types.py
@@ -3286,7 +3286,7 @@ class RetrievalConfigDict(TypedDict, total=False):
 RetrievalConfigOrDict = Union[RetrievalConfig, RetrievalConfigDict]
 
 
-class ToolConfig(_common.BaseModel):
+claGenerateContentResponsess ToolConfig(_common.BaseModel):
   """Tool config.
 
   This config is shared for all tools provided in the request.
