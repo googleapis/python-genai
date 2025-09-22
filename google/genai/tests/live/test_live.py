e8dcd7eb6a09a8528386d3b04ac9c362d710dc9f
Jaycee Li <jayceeli@google.com>
2025-09-22 15:40:28 -0700

    chore: Remove no-op converters
    PiperOrigin-RevId: 810180941

--- google/genai/tests/live/test_live.py
+++ google/genai/tests/live/test_live.py
@@ -626,15 +626,10 @@ async def test_bidi_setup_to_api_speech_config(vertexai):
           'model': 'models/test_model',
           'generationConfig': {
               'speechConfig': {
-                  # Note: the snake_casing is different from the usual camelCase
-                  # here. This is because speechConfig is an unmodified proto
-                  # defined in the discovery doc, so it doesn't need to/from
-                  # converters. The API is insensitive to the case format.
-                  # This looks wrong, but it is okay/correct.
-                  'voiceConfig': {
-                      'prebuiltVoiceConfig': {'voiceName': 'en-default'}
+                  'voice_config': {
+                      'prebuilt_voice_config': {'voice_name': 'en-default'}
                   },
-                  'languageCode': 'en-US',
+                  'language_code': 'en-US',
               },
               'enableAffectiveDialog': True,
               'temperature': 0.7,
@@ -644,7 +639,7 @@ async def test_bidi_setup_to_api_speech_config(vertexai):
               'mediaResolution': 'MEDIA_RESOLUTION_MEDIUM',
               'seed': 13,
           },
-          'proactivity': {'proactiveAudio': True},
+          'proactivity': {'proactive_audio': True},
           'systemInstruction': {
               'parts': [
                   {
@@ -663,6 +658,12 @@ async def test_bidi_setup_to_api_speech_config(vertexai):
     expected_result['setup']['generationConfig']['responseModalities'] = [
         'AUDIO'
     ]
+    expected_result['setup']['generationConfig']['speechConfig'] = {
+        'voiceConfig': {
+            'prebuilt_voice_config': {'voice_name': 'en-default'}
+        },
+        'languageCode': 'en-US',
+    }
   else:
     expected_result['setup']['model'] = 'models/test_model'
 
@@ -909,8 +910,8 @@ async def test_bidi_setup_to_api_with_context_window_compression(
               'role': 'user',
           },
            'contextWindowCompression': {
-              'triggerTokens': 1000,
-              'slidingWindow': {'targetTokens': 10},
+              'trigger_tokens': 1000,
+              'sliding_window': {'target_tokens': 10},
           }
       }
   }
@@ -1284,17 +1285,7 @@ async def test_bidi_setup_to_api_with_realtime_input_config(vertexai):
   expected_result = {
       'setup': {
           'model': 'test_model',
-          'realtimeInputConfig': {
-              'automaticActivityDetection': {
-                  'disabled': True,
-                  'startOfSpeechSensitivity': 'START_SENSITIVITY_HIGH',
-                  'endOfSpeechSensitivity': 'END_SENSITIVITY_HIGH',
-                  'prefixPaddingMs': 20,
-                  'silenceDurationMs': 100,
-              },
-              'activityHandling': 'NO_INTERRUPTION',
-              'turnCoverage': 'TURN_INCLUDES_ALL_INPUT',
-          },
+          'realtimeInputConfig': config_dict['realtime_input_config'],
       }
   }
 
