614927184e70202947ae5a292ac52accf16da91d
Matthew Tang <tangmatthew@google.com>
2025-10-15 14:29:46 -0700

    feat: Add safety_filter_level and person_generation for Imagen upscaling
    PiperOrigin-RevId: 819915543

--- google/genai/types.py
+++ google/genai/types.py
@@ -7285,6 +7285,12 @@ class _UpscaleImageAPIConfig(_common.BaseModel):
       default=None,
       description="""Cloud Storage URI used to store the generated images.""",
   )
+  safety_filter_level: Optional[SafetyFilterLevel] = Field(
+      default=None, description="""Filter level for safety filtering."""
+  )
+  person_generation: Optional[PersonGeneration] = Field(
+      default=None, description="""Allows generation of people by the model."""
+  )
   include_rai_reason: Optional[bool] = Field(
       default=None,
       description="""Whether to include a reason for filtered-out images in the
@@ -7332,6 +7338,12 @@ class _UpscaleImageAPIConfigDict(TypedDict, total=False):
 
   output_gcs_uri: Optional[str]
   """Cloud Storage URI used to store the generated images."""
+
+  safety_filter_level: Optional[SafetyFilterLevel]
+  """Filter level for safety filtering."""
+
+  person_generation: Optional[PersonGeneration]
+  """Allows generation of people by the model."""
 
   include_rai_reason: Optional[bool]
   """Whether to include a reason for filtered-out images in the
@@ -13232,6 +13244,12 @@ class UpscaleImageConfig(_common.BaseModel):
       default=None,
       description="""Cloud Storage URI used to store the generated images.""",
   )
+  safety_filter_level: Optional[SafetyFilterLevel] = Field(
+      default=None, description="""Filter level for safety filtering."""
+  )
+  person_generation: Optional[PersonGeneration] = Field(
+      default=None, description="""Allows generation of people by the model."""
+  )
   include_rai_reason: Optional[bool] = Field(
       default=None,
       description="""Whether to include a reason for filtered-out images in the
@@ -13278,6 +13296,12 @@ class UpscaleImageConfigDict(TypedDict, total=False):
 
   output_gcs_uri: Optional[str]
   """Cloud Storage URI used to store the generated images."""
+
+  safety_filter_level: Optional[SafetyFilterLevel]
+  """Filter level for safety filtering."""
+
+  person_generation: Optional[PersonGeneration]
+  """Allows generation of people by the model."""
 
   include_rai_reason: Optional[bool]
   """Whether to include a reason for filtered-out images in the
