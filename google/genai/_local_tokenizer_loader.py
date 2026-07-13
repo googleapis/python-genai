4631f5e0397ea0760f266ebfd479269df6286958
Wanlin Du <wanlindu@google.com>
2026-07-13 11:11:41 -0700

    chore: Update the ci to use sentencepiece==0.2.1
    PiperOrigin-RevId: 947139792

--- google/genai/_local_tokenizer_loader.py
+++ google/genai/_local_tokenizer_loader.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+# Thsi is to test if it failed on HEAD
 
 import dataclasses
 import functools
