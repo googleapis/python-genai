2a9350d1f3f38e13738ab1527357810aef9cea45
Yvonne Yu <yyyu@google.com>
2026-07-14 11:38:06 -0700

    test: add tests for private chat
    PiperOrigin-RevId: 947813185

--- google/genai/tests/private/test_send_message_stream_private.py
+++ google/genai/tests/private/test_send_message_stream_private.py
@@ -155,3 +155,119 @@ def test_send_message_stream_function_tool_afc_enabled_multi_turn(client):
     assert '1000' in history[11].parts[0].text
     assert history[12].role == 'model'
     assert history[12].parts[0].text == ''
+
+
+@pytest.mark.asyncio
+async def test_async_send_message_stream_function_tool_afc_disabled(client):
+  chat = client.aio.chats.create(
+      model=MODEL_NAME,
+      config={
+          'tools': [get_weather],
+      },
+  )
+  async for chunk in await chat.send_message_stream(
+      'What is the weather in Boston?'
+  ):
+    pass
+  history = chat.get_history()
+  assert len(history) == 3
+  assert history[0].role == 'user'
+  assert history[1].role == 'model'
+  assert history[1].parts[0].function_call.name == 'get_weather'
+  assert history[2].role == 'model'
+  assert history[2].parts[0].text == ''
+
+
+@pytest.mark.asyncio
+async def test_async_send_message_stream_function_tool_afc_enabled(client):
+  chat = client.aio.chats.create(
+      model=MODEL_NAME,
+      config={
+          'tools': [get_weather],
+          'automatic_function_calling': {'enable': True},
+      },
+  )
+  async for chunk in await chat.send_message_stream(
+      'What is the weather in Boston?'
+  ):
+    pass
+  history = chat.get_history()
+  if client.vertexai:
+    assert len(history) == 6
+    assert history[0].role == 'user'
+    assert history[1].role == 'model'
+    assert history[1].parts[0].function_call.name == 'get_weather'
+    assert history[2].role == 'model'
+    assert history[2].parts[0].text == ''
+    assert history[3].role == 'user'
+    assert history[3].parts[0].function_response.name == 'get_weather'
+    assert history[4].role == 'model'
+    assert 'Boston' in history[4].parts[0].text
+    assert history[5].parts[0].text == ''
+  else:
+    assert len(history) == 7
+    assert history[0].role == 'user'
+    assert history[1].role == 'model'
+    assert history[1].parts[0].function_call.name == 'get_weather'
+    assert history[2].role == 'model'
+    assert history[2].parts[0].text == ''
+    assert history[3].role == 'user'
+    assert history[3].parts[0].function_response.name == 'get_weather'
+    assert history[4].role == 'model'
+    assert 'Boston' in history[4].parts[0].text
+    assert history[5].role == 'model'
+    assert 'degrees' in history[5].parts[0].text
+    assert history[6].role == 'model'
+    assert history[6].parts[0].text == ''
+
+
+@pytest.mark.asyncio
+async def test_async_send_message_stream_function_tool_afc_enabled_multi_turn(
+    client,
+):
+  chat = client.aio.chats.create(
+      model=MODEL_NAME,
+      config={
+          'tools': [get_weather, get_stock_price],
+          'automatic_function_calling': {'enable': True},
+      },
+  )
+  async for _ in await chat.send_message_stream(
+      'What is the weather in Boston?'
+  ):
+    pass
+  history = chat.get_history()
+
+  assert len(history) == 6
+  assert history[0].role == 'user'
+  assert history[1].role == 'model'
+  assert history[1].parts[0].function_call.name == 'get_weather'
+  assert history[2].role == 'model'
+  assert history[2].parts[0].text == ''
+  assert history[3].role == 'user'
+  assert history[3].parts[0].function_response.name == 'get_weather'
+  assert history[4].role == 'model'
+  assert 'Boston' in history[4].parts[0].text
+  assert history[5].role == 'model'
+  assert history[5].parts[0].text == ''
+
+  async for _ in await chat.send_message_stream(
+      'What is the stock price of symbol GOOG?'
+  ):
+    pass
+  history = chat.get_history()
+
+  assert len(history) == 13
+  assert history[6].role == 'user'
+  assert history[7].role == 'model'
+  assert history[7].parts[0].function_call.name == 'get_stock_price'
+  assert history[8].role == 'model'
+  assert history[8].parts[0].text == ''
+  assert history[9].role == 'user'
+  assert history[9].parts[0].function_response.name == 'get_stock_price'
+  assert history[10].role == 'model'
+  assert 'stock' in history[10].parts[0].text
+  assert history[11].role == 'model'
+  assert '1000' in history[11].parts[0].text
+  assert history[12].role == 'model'
+  assert history[12].parts[0].text == ''
